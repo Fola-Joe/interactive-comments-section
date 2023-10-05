ad7a013fea38b07e42f0d3938cd7113c8ac8f8c3
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-10-05 12:49:39 +0100

    Comment section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
+import './App.css';
+import Comment from './Comment';
 
 function App() {
 
   return (
-    <div>
-      
+    <div className="App-main">
+      <Comment />
     </div>
   )
 }
---
+++ src/Comment.jsx
@@ -0,0 +1,30 @@
+import './Comment.css';
+
+export default function Comment() {
+    return (
+        <div className="Comment">
+            <section className="comment-top">
+                <img src="/avatars/image-amyrobson.png" alt="avatar icon" />
+                <h1>amyrobson</h1>
+                <h2>1 month ago</h2>
+            </section>
+            <p className="comment-writeup">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Neque reiciendis sapiente voluptatibus officia cumque aut atque,
+                qui soluta consequatur perspiciatis, aperiam hic cum dignissimos
+                distinctio quis est provident. Commodi, quasi!
+            </p>
+            <div className="comment-bottom">
+                <section className="vote-section">
+                    <img className="vote plus" src="/icon-plus.svg" alt="plus icon" />
+                    <h3 className="vote number">12</h3>
+                    <img className="vote minus" src="/icon-minus.svg" alt="minus icon" />
+                </section>
+                <section  className="reply-section">
+                    <img src="/icon-reply.svg" alt="reply icon" />
+                    <h4>Reply</h4>
+                </section>
+            </div>
+        </div>
+    )
+}
