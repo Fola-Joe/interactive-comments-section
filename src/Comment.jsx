82e0366e23ab41c879d3d7a8bcc3be6b630a4fac
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-10-09 23:29:02 +0100

    inclusion of comments asides replies

--- src/Comment.jsx
+++ src/Comment.jsx
@@ -1,26 +1,21 @@
 import './Comment.css';
 
-export default function Comment() {
+export default function Comment({ username, content, image, time, score }) {
     return (
         <div className="Comment">
             <section className="comment-top">
-                <img src="/avatars/image-amyrobson.png" alt="avatar icon" />
-                <h1>amyrobson</h1>
-                <h2>1 month ago</h2>
+                <img src={image} alt="user avatar" />
+                <h1>{username}</h1>
+                <h2>{time}</h2>
             </section>
-            <p className="comment-writeup">
-                Impressive! Though it seems Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Neque reiciendis sapiente voluptatibus officia cumque aut atque,
-                qui soluta consequatur perspiciatis, aperiam hic cum dignissimos
-                distinctio quis est provident. Commodi, quasi!
-            </p>
+            <p className="comment-writeup">{content}</p>
             <div className="comment-bottom">
                 <section className="vote-section">
                     <img className="vote plus" src="/icon-plus.svg" alt="plus icon" />
-                    <h3 className="vote number">12</h3>
+                    <h3 className="vote number">{score}</h3>
                     <img className="vote minus" src="/icon-minus.svg" alt="minus icon" />
                 </section>
-                <section  className="reply-section">
+                <section className="reply-section">
                     <img src="/icon-reply.svg" alt="reply icon" />
                     <h4>Reply</h4>
                 </section>
--- src/CommentList.jsx
+++ src/CommentList.jsx
@@ -2,10 +2,22 @@ import Comment from "./Comment";
 import data from "./data.json";
 
 export default function CommentList() {
+    const { comments } = data;
+    // Extract the comments array from the JSON data
 
     return (
         <div className="CommentList">
-            <Comment />
+            {comments.map((comment) => (
+                <Comment
+                    key={comment.id}
+                    username={comment.user.username}
+                    content={comment.content}
+                    image={comment.user.image.png}
+                    time={comment.createdAt}
+                    score={comment.score}
+                />
+            ))}
+            {/* <Comment /> */}
         </div>
     )
 }
