cfe57d14bb0f59a96e8bf1bc8d0fcea402136a27
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-10-11 11:32:18 +0100

    update on votes per comment/replies

--- src/ReplyComment.jsx
+++ src/ReplyComment.jsx
@@ -1,6 +1,19 @@
 import './Comment.css';
+import { useState } from 'react';
 
 export default function ReplyComment({ username, reply, image, time, score }) {
+    const [newScore, setNewScore] = useState(score);
+
+    // function to update votes
+    const updateScore = (value) => {
+        // Calculate the updated score
+        const updatedScore = newScore + value;
+
+        // Prevent the score from going below 0
+        const nonNegativeScore = Math.max(updatedScore, 0);
+
+        setNewScore(nonNegativeScore);
+    };
     return (
         <div className="Comment">
             <section className="comment-top">
@@ -11,9 +24,19 @@ export default function ReplyComment({ username, reply, image, time, score }) {
             <p className="comment-writeup">{reply}</p>
             <div className="comment-bottom">
                 <section className="vote-section">
-                    <img className="vote plus" src="/icon-plus.svg" alt="plus icon" />
-                    <h3 className="vote number">{score}</h3>
-                    <img className="vote minus" src="/icon-minus.svg" alt="minus icon" />
+                    <img
+                        className="vote plus"
+                        src="/icon-plus.svg"
+                        alt="plus icon"
+                        onClick={() => updateScore(1)}
+                    />
+                    <h3 className="vote number">{newScore}</h3>
+                    <img
+                        className="vote minus"
+                        src="/icon-minus.svg"
+                        alt="minus icon"
+                        onClick={() => updateScore(-1)}
+                    />
                 </section>
                 <section className="reply-section">
                     <img src="/icon-reply.svg" alt="reply icon" />
